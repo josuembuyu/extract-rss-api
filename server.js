3353fc37c56234ab26a6e8b061e9788d5db7a9d2
josuembuyu6 <josuembuyu94@gmail.com>
2024-04-23 17:35:04 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const cheerio = require("cheerio");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors()); // Allow all CORS requests
+
+app.get("/api/rss", async (req, res) => {
+  try {
+    const url = req.query.url;
+    if (!url) {
+      return res.status(400).json({ error: "URL parameter is required" });
+    }
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: `Failed to fetch resource: ${response.statusText}` });
+    }
+
+    const html = await response.text();
+    const $ = cheerio.load(html);
+
+    const title = $("title").text();
+    console.log("Title:", title);
+
+    const rssLink = $('link[rel="alternate"][type="application/rss+xml"]').attr(
+      "href"
+    );
+    res.json({ rssLink, title });
+  } catch (error) {
+    console.error("Error fetching or parsing RSS:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
